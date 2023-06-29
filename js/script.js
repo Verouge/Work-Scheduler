30ae831b803729e531ab00bf08df19a3a20dfa0b
Verouge <branola1998628@gmail.com>
2023-06-29 18:34:58 +1000

    Finalised Code for JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,78 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Display the current date at the top of the calendar
+  const currentDate = dayjs().format("MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+
+  // Generate time blocks from 9am to 5pm
+  const startTime = 9; // Start time (9am)
+  const endTime = 17; // End time (5pm)
+
+  for (let hour = startTime; hour <= endTime; hour++) {
+    const timeBlockId = "hour-" + hour;
+
+    const $timeBlock = $("<div>")
+      .attr("id", timeBlockId)
+      .addClass("row time-block");
+
+    const amPm = hour >= 12 ? "PM" : "AM";
+    const displayHour = hour > 12 ? hour - 12 : hour;
+    const $hourDiv = $("<div>")
+      .addClass("col-2 col-md-1 hour text-center py-3")
+      .text(displayHour + amPm);
+
+    const $descriptionTextarea = $("<textarea>")
+      .addClass("col-8 col-md-10 description")
+      .attr("rows", "3");
+
+    const $saveButton = $("<button>")
+      .addClass("btn saveBtn col-2 col-md-1")
+      .attr("aria-label", "save");
+    const $saveIcon = $("<i>")
+      .addClass("fas fa-save")
+      .attr("aria-hidden", "true");
+
+    $saveButton.append($saveIcon);
+
+    $timeBlock.append($hourDiv, $descriptionTextarea, $saveButton);
+    $(".container-fluid").append($timeBlock);
+  }
+
+  // Add click event listener to the save buttons
+  $(".saveBtn").on("click", function () {
+    const $timeBlock = $(this).closest(".time-block");
+    const timeBlockId = $timeBlock.attr("id");
+    const description = $timeBlock.find(".description").val();
+    localStorage.setItem(timeBlockId, description);
+
+    const $confirmationMessage = $("<p>")
+      .text("Appointment added to Local Storage")
+      .addClass("confirmation-message");
+
+    $("header").append($confirmationMessage);
+
+    setTimeout(function () {
+      $confirmationMessage.remove();
+    }, 1000);
+  });
+
+  // Load saved events from local storage and set the values of the corresponding textarea elements
+  $(".time-block").each(function () {
+    const timeBlockId = $(this).attr("id");
+    const description = localStorage.getItem(timeBlockId);
+    $(this).find(".description").val(description);
+  });
+
+  // Add "past," "present," or "future" class to each time block
+  const currentHour = dayjs().hour();
+  $(".time-block").each(function () {
+    const timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (timeBlockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+  });
 });
